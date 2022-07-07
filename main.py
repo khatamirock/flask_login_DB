d563afe0b271d7739e6d5b73e13763e71b63a61e
md. khatami <67198296+khatamirock@users.noreply.github.com>
2022-07-07 19:42:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from web import Create_app
 app = Create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-#     app.run(debug=True)
+#     app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
